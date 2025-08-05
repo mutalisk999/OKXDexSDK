70b94d0421c6920556bb20702d3a3dda3ca3ed1a
mutalisk999 <81849682@qq.com>
2025-08-05 16:57:19 +0800

    aggregator quote & aggregator history

--- api/__init__.py
+++ api/__init__.py
@@ -2,7 +2,7 @@
 # encoding: utf-8
 
 import os
-
+from enum import StrEnum
 import aiohttp
 import asyncio
 from aiohttp_socks import ProxyConnector
@@ -11,6 +11,11 @@ from dotenv import load_dotenv
 from utils import get_time_now_iso_8601, calc_ok_access_sign
 
 DOMAIN_NAME = "https://web3.okx.com"
+
+
+class SwapMode(StrEnum):
+    exactIn: str = "exactIn"
+    exactOut: str = "exactOut"
 
 
 # chain
@@ -123,6 +128,65 @@ async def get_approve_transaction(api_key: str, secret_key: str, pass_phrase: st
             return await response.json()
 
 
+# quote
+async def get_aggregator_quote(api_key: str, secret_key: str, pass_phrase: str,
+                               chain_idx: int, swap_mode: SwapMode, amount: int,
+                               from_token_contract_addr: str, to_token_contract_addr: str,
+                               timeout: int) -> dict:
+    # SwapMode: exactIn => amount: sell exact amount
+    # SwapMode: exactOut => amount: buy exact amount
+    request_path = "/api/v5/dex/aggregator/quote?chainIndex={}&swapMode={}&amount={}&fromTokenAddress={}&toTokenAddress={}".format(
+        chain_idx, swap_mode.value, amount, from_token_contract_addr, to_token_contract_addr)
+    ts_iso_8601 = get_time_now_iso_8601()
+    ok_access_sign = calc_ok_access_sign(secret_key, ts_iso_8601, "GET", request_path)
+    headers = {
+        "OK-ACCESS-KEY": api_key,
+        "OK-ACCESS-SIGN": ok_access_sign,
+        "OK-ACCESS-PASSPHRASE": pass_phrase,
+        "OK-ACCESS-TIMESTAMP": ts_iso_8601,
+    }
+    url = "".join([DOMAIN_NAME, request_path])
+
+    connector = None
+    proxy_url = os.getenv("CLIENT_PROXY")
+
+    if proxy_url is not None and proxy_url != "":
+        connector = ProxyConnector.from_url(proxy_url)
+
+    client_timeout = aiohttp.ClientTimeout(total=timeout)
+
+    async with aiohttp.ClientSession(connector=connector) as session:
+        async with session.get(url, headers=headers, timeout=client_timeout) as response:
+            return await response.json()
+
+
+# history
+async def get_aggregator_history(api_key: str, secret_key: str, pass_phrase: str,
+                                 chain_idx: int, tx_hash: str, timeout: int) -> dict:
+    request_path = "/api/v5/dex/aggregator/history?chainIndex={}&txHash={}".format(chain_idx, tx_hash)
+    ts_iso_8601 = get_time_now_iso_8601()
+    ok_access_sign = calc_ok_access_sign(secret_key, ts_iso_8601, "GET", request_path)
+    headers = {
+        "OK-ACCESS-KEY": api_key,
+        "OK-ACCESS-SIGN": ok_access_sign,
+        "OK-ACCESS-PASSPHRASE": pass_phrase,
+        "OK-ACCESS-TIMESTAMP": ts_iso_8601,
+    }
+    url = "".join([DOMAIN_NAME, request_path])
+
+    connector = None
+    proxy_url = os.getenv("CLIENT_PROXY")
+
+    if proxy_url is not None and proxy_url != "":
+        connector = ProxyConnector.from_url(proxy_url)
+
+    client_timeout = aiohttp.ClientTimeout(total=timeout)
+
+    async with aiohttp.ClientSession(connector=connector) as session:
+        async with session.get(url, headers=headers, timeout=client_timeout) as response:
+            return await response.json()
+
+
 if __name__ == "__main__":
     async def func(api_key: str, secret_key: str, pass_phrase: str):
         timeout = 10
@@ -140,10 +204,31 @@ if __name__ == "__main__":
             "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", "100000", timeout)
         print(r)
 
+        r = await get_aggregator_quote(api_key, secret_key, pass_phrase, 1, SwapMode.exactIn,
+                                       10000000000000000000,
+                                       "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+                                       "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", timeout)
+        print(r)
+
+        r = await get_aggregator_history(api_key, secret_key, pass_phrase, 784,
+                                         "5GePcvqEakoUtArW8PHULDSQds95vcgeiTznvbnb8hCV", timeout)
+        print(r)
+
+
+    async def func2(api_key: str, secret_key: str, pass_phrase: str):
+        timeout = 10
+        fut1 = get_aggregator_supported_chain(api_key, secret_key, pass_phrase, 1, timeout)
+        fut2 = get_aggregator_history(api_key, secret_key, pass_phrase, 784,
+                                      "5GePcvqEakoUtArW8PHULDSQds95vcgeiTznvbnb8hCV", timeout)
+        r1, r2 = await asyncio.gather(fut1, fut2)
+        print(r1)
+        print(r2)
+
 
     load_dotenv("../.env")
     apikey = str(os.getenv("API_KEY"))
     secretkey = str(os.getenv("API_SECRET"))
     passphrase = str(os.getenv("API_PASSPHRASE"))
 
-    asyncio.run(func(apikey, secretkey, passphrase))
+    # asyncio.run(func(apikey, secretkey, passphrase))
+    asyncio.run(func2(apikey, secretkey, passphrase))
